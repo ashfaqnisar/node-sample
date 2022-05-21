0689af220909134767671afd8989045399812761
Ashfaq <ashfaqnisar00@gmail.com>
2022-05-21 17:33:52 +0530

    initial commit

---
+++ test.js
@@ -0,0 +1,9 @@
+import { add } from "./functions.js"
+import chalk from "chalk"
+
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.green('Hello world!'));
+// console.log(chalk.redishGreen('Hello world!'));
+
+console.log(add(12, 2));
+console.log("dfsddfddf")
